804a40ac47bc4fc1d38871ccd1d97b8d5218140f
Edgar Villegas Alvarado <edgarinvillegas@hotmail.com>
2018-06-06 01:28:02 -0400

    feat: Finish Lecture 14. Test and build the Gift Component
    Add person input change simulation

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App', () => {
+    console.log('-------------------------------------------------------');
     const app = shallow(<App />);
 
     it("renders correctly", () => {
